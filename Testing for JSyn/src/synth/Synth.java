7fad5f31e27657b29208d15ad0977d4a67466dda
SatiricRug <pjschinske@gmail.com>
2015-10-12 19:06:05 -0700

    asdf

--- Testing for JSyn/src/synth/Synth.java
+++ Testing for JSyn/src/synth/Synth.java
@@ -3,12 +3,15 @@ package synth;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JToggleButton;
 
 import com.jsyn.JSyn;
 import com.jsyn.Synthesizer;
@@ -20,12 +23,13 @@ import com.jsyn.unitgen.LineOut;
 import com.jsyn.unitgen.SineOscillator;
 
 public class Synth extends JFrame {
-
+	
+	JPanel;
+	
 	private static final long serialVersionUID = 1L;
 	private Synthesizer synth;
 	
 	public Synth() {
-		
 		synth = JSyn.createSynthesizer();
 		LineOut lineOut = new LineOut();
 		SineOscillator osc = new SineOscillator();
@@ -47,6 +51,28 @@ public class Synth extends JFrame {
 		setupPortKnob(osc.frequency);
 		setupPortKnob(osc.amplitude);
 		
+		JToggleButton[] noteToggleButtons = new JToggleButton[16];
+		
+		ItemListener toggleButtonClicked = new ItemListener() {
+			
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if (e.getStateChange() == ItemEvent.SELECTED)  {
+					
+				} else if (e.getStateChange() == ItemEvent.DESELECTED) {
+					
+				}
+			}
+			
+		};
+		
+		for(int i = 0; i < noteToggleButtons.length; i++) {
+			noteToggleButtons[i] = new JToggleButton(Integer.toString(i));
+			noteToggleButtons[i].addItemListener(toggleButtonClicked);
+			noteToggleButtons[i].setBounds(10 + i * 20, 280, 10, 10);
+			add(noteToggleButtons[i]);
+		}
+		
 		synth.start();
 		lineOut.start();
 	}
@@ -57,15 +83,16 @@ public class Synth extends JFrame {
 		RotaryTextController knob = new RotaryTextController(model, 10);
 		knob.setBorder( BorderFactory.createTitledBorder(port.getName()));
 		knob.setTitle(port.getName());
-		add(knob);
+		//add(knob);
 	}
 	
 	public static void main(String[] args) {
 		JFrame frame = new Synth();
+		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Synth");
-		frame.setSize(300, 300);
-		frame.setLocation(640, 350);
+		frame.setSize(340, 300);
+		frame.setLocation(710, 350);
 		frame.setVisible(true);
 	}
 	
